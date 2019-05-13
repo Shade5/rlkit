a061a8837e04dada5e1256497d6352857ce77ef8
Shade5 <thrallin@yahoo.com>
2019-05-12 20:27:53 -0400

    Moved flex_wrapper to correct location. Added trained goal runner for flex

--- examples/her/her_sac_flex_fetch_reach.py
+++ examples/her/her_sac_flex_fetch_reach.py
@@ -12,8 +12,7 @@ from rlkit.torch.networks import FlattenMlp
 from rlkit.torch.sac.policies import MakeDeterministic, TanhGaussianPolicy
 from rlkit.torch.sac.sac import SACTrainer
 from rlkit.torch.torch_rl_algorithm import TorchBatchRLAlgorithm
-
-from flex_wrappers import FetchReach
+from rlkit.core.flex_wrappers import FetchReach
 
 
 def experiment(variant):
---
+++ scripts/run_goal_conditioned_flex.py
@@ -0,0 +1,54 @@
+import argparse
+import pickle
+import sys
+sys.path.append('.')
+import rlkit.torch.pytorch_util as ptu
+ptu.set_gpu_mode(True)
+from rlkit.core import logger
+from rlkit.samplers.rollout_functions import multitask_rollout
+from rlkit.torch import pytorch_util as ptu
+from rlkit.envs.vae_wrapper import VAEWrappedEnv
+from rlkit.core.flex_wrappers import FetchReach
+
+def simulate_policy(args):
+    data = pickle.load(open(args.file, "rb"))
+    policy = data['evaluation/policy']
+    env = FetchReach()
+    print("Policy and environment loaded")
+    if args.gpu:
+        ptu.set_gpu_mode(True)
+        policy.to(ptu.device)
+    paths = []
+    while True:
+        paths.append(multitask_rollout(
+            env,
+            policy,
+            max_path_length=args.H,
+            observation_key='observation',
+            desired_goal_key='desired_goal',
+        ))
+        if hasattr(env, "log_diagnostics"):
+            env.log_diagnostics(paths)
+        if hasattr(env, "get_diagnostics"):
+            for k, v in env.get_diagnostics(paths).items():
+                logger.record_tabular(k, v)
+        logger.dump_tabular()
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('file', type=str,
+                        help='path to the snapshot file')
+    parser.add_argument('--H', type=int, default=300,
+                        help='Max length of rollout')
+    parser.add_argument('--speedup', type=float, default=10,
+                        help='Speedup')
+    parser.add_argument('--mode', default='video_env', type=str,
+                        help='env mode')
+    parser.add_argument('--gpu', action='store_true')
+    parser.add_argument('--enable_render', action='store_true')
+    parser.add_argument('--hide', action='store_true')
+    args = parser.parse_args()
+
+    simulate_policy(args)
